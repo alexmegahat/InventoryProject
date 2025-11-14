21c8449c70467ddeecca142fa513952f4e9ea44b
al <makisevaleksej537@gmail.com>
2025-11-14 19:02:54 +0600

    add EquipmentComponent
    - create EquipmentComponent
    - add it to the player controller
    - bind to OnItemEquipped/Unequipped delegates

---
+++ Plugins/Inventory/Source/Inventory/Private/EquipmentManagement/Components/Inv_EquipmentComponent.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EquipmentManagement/Components/Inv_EquipmentComponent.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/PlayerController.h"
+#include "InventoryManagement/Components/Inv_InventoryComponent.h"
+#include "InventoryManagement/Utils/Inv_InventoryStatics.h"
+
+
+void UInv_EquipmentComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	OwningPlayerController = Cast<APlayerController>(GetOwner());
+	
+	if (OwningPlayerController.IsValid())
+	{
+		if (ACharacter* PlayerPawn = Cast<ACharacter>(OwningPlayerController->GetPawn()); IsValid(PlayerPawn))
+		{
+			OwningSkeletalMesh = PlayerPawn->GetMesh();
+			
+			InitInventoryComponent();
+		}
+	}
+}
+
+void UInv_EquipmentComponent::OnItemEquipped(UInv_InventoryItem* EquippedItem)
+{
+	
+}
+
+void UInv_EquipmentComponent::OnItemUnequipped(UInv_InventoryItem* UnequippedItem)
+{
+	
+}
+
+void UInv_EquipmentComponent::InitInventoryComponent()
+{
+	InventoryComponent = UInv_InventoryStatics::GetInventoryComponent(OwningPlayerController.Get());
+	if (!InventoryComponent.IsValid()) return;
+
+	//Bind Item Equipped
+	if (!InventoryComponent->OnItemEquipped.IsAlreadyBound(this, &ThisClass::OnItemEquipped))
+	{
+		InventoryComponent->OnItemEquipped.AddDynamic(this, &ThisClass::OnItemEquipped);
+	}
+
+	//Bind Item Unequipped
+	if (!InventoryComponent->OnItemEquipped.IsAlreadyBound(this, &ThisClass::OnItemUnequipped))
+	{
+		InventoryComponent->OnItemEquipped.AddDynamic(this, &ThisClass::OnItemUnequipped);
+	}
+}
+
---
+++ Plugins/Inventory/Source/Inventory/Public/EquipmentManagement/Components/Inv_EquipmentComponent.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Inv_EquipmentComponent.generated.h"
+
+class UInv_InventoryItem;
+class UInv_InventoryComponent;
+class APlayerController;
+class USkeletalMeshComponent;
+
+/*
+ * Equipment Component - responsible for spawning in actors associated with inventory items and
+ * attaching them to the owning pawn's skeletal mesh.
+ * 
+ */
+UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class INVENTORY_API UInv_EquipmentComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	TWeakObjectPtr<APlayerController> OwningPlayerController;
+
+	TWeakObjectPtr<USkeletalMeshComponent> OwningSkeletalMesh;
+	
+	TWeakObjectPtr<UInv_InventoryComponent> InventoryComponent;
+	
+	UFUNCTION()
+	void OnItemEquipped(UInv_InventoryItem* EquippedItem);
+
+	UFUNCTION()
+	void OnItemUnequipped(UInv_InventoryItem* UnequippedItem);
+
+	//called in OnBeginPlay
+	void InitInventoryComponent();
+};
